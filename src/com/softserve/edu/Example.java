491594363b1c365f3a741d2b4d57073d3d061097
inady <naduchtest@gmail.com>
2020-03-06 13:55:57 +0200

    Add some intro to the inadytest branch

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -3,6 +3,7 @@ package com.softserve.edu;
 public class Example {
 
     public static void main(String[] args)   {
+    	System.out.printf("Hi there!!\nThis is Ivan's test branch and I'm going to work here:)\n");
         System.out.println("My first program");
         //
         for (int i=0; i<args.length; i++) {
