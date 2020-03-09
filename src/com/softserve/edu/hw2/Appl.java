50f9424d32f535bca63309446a5f0991a6cb0d9a
Vlad <dixi.bah@gmail.com>
2020-03-09 16:08:08 +0200

    Created new class

---
+++ src/com/softserve/edu/hw2/Appl.java
@@ -0,0 +1,22 @@
+package com.softserve.edu.hw2;
+
+import java.util.Scanner;
+
+public class Appl {
+    public static void main(String[] args)   {
+        int k = 16;
+
+        System.out.println("Value of k++ = " + (k++)); //k = 16
+        System.out.println("Value of ++k = " + (++k)); //k = 18
+        System.out.println("Value of k+++k = " + (k + ++k)); //k = 18 + 19 = 37
+        System.out.println("Value of 50---k = " + (50 - --k)); //k = 50 - 18 = 32
+        System.out.println("Value of k*k = " + (k*k)); //k = 18 * 18 = 342
+        System.out.println("Value of k/3 = " + (k/3)); //k = 18 / 3 = 6
+        System.out.println("Value of k++<++k = " + (k++ < ++k)); // 18 < 20
+
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
