ad79c29bc6b642ed259fff7c7673e1fda1a9c9e8
nnadt <nadtocha.natalya@gmail.com>
2020-03-09 16:26:46 +0200

    Added new operation

--- src/com/softserve/edu/hw2/Appl.java
+++ src/com/softserve/edu/hw2/Appl.java
@@ -12,6 +12,7 @@ public class Appl {
         System.out.println("Value of 50---k = " + (50 - --k)); //k = 50 - 18 = 32
         System.out.println("Value of k*k = " + (k*k)); //k = 18 * 18 = 342
         System.out.println("Value of k/3 = " + (k/3)); //k = 18 / 3 = 6
+        System.out.println("Value of k%3 = " + (k%3)); //k = 18 % 3 = 0
         System.out.println("Value of k++<++k = " + (k++ < ++k)); // 18 < 20
 
         System.out.println("What is your name?");
