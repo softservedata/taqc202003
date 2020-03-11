e2a8dc6688bbf04a1b0d13636299586b1ea08296
Oleksiy Makhynia <makhynia.o.m@gmail.com>
2020-03-11 20:15:13 +0200

    Update Value

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -6,7 +6,7 @@ public class Example {
         System.out.println("My first program");
         //
         for (int i=0; i<args.length; i++) {
-            System.out.println("Argument i = " + i + "  value = " + args[i]);  
+            System.out.println("Argument i = " + i + "  Value = " + args[i]);  
         }
     }
 
