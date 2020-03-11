a2c2eba25e74b20513a3354396874912d4d11465
olukom <olukom@softserveinc.com>
2020-03-11 23:41:05 +0200

    result of k++

---
+++ src/com/softserve/edu/hw2/Appl.java
@@ -0,0 +1,12 @@
+package com.softserve.edu.hw2;
+
+public class Appl {
+	public static void main(String args[]) {
+
+        int k=16;
+      
+        int result =++k; // 
+
+        System.out.println("Result of K = " + k);
+      }
+  }
