1380f772910393736312486e1c6dac08b8b0c168
vvkushnir <vikkushniruk@gmail.com>
2020-03-12 01:58:11 +0200

    My test class Appl

---
+++ src/com/softserve/edu/Appl.java
@@ -0,0 +1,30 @@
+package com.softserve.edu;
+import java.util.Scanner;
+public class Appl {
+    public static void main(String[] args) {
+
+            int k = 16;
+            /*   k++; ++k; k+++k; 50---k; k*k; k/3; k%3; k++<++k;    */
+
+            System.out.println("k++= " + + (k++  ));
+
+            System.out.println("++k= " + + (++k  ));
+
+            System.out.println("k+++k " + (k+++k  ));
+
+           /* System.out.println("50---k " + (50---k  ));*/
+
+            System.out.println("k*k= " + (k*k  ));
+
+            System.out.println("k/3= " + (k/3  ));
+
+            System.out.println("k%3= " + (k%3  ));
+
+            System.out.println("k%3= " + (k++<++k  ));
+
+            System.out.println("What is your name?");
+            Scanner name = new Scanner( System. in);
+            String userName = name.nextLine();
+            System.out.println("Hello " + userName);
+    }
+}
