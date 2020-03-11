42e9ebd14ef5cd4b67ecc257285c03abb3e4cb3f
vvkushnir <vikkushniruk@gmail.com>
2020-03-12 00:51:43 +0200

    My test11 program

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -3,7 +3,7 @@ package com.softserve.edu;
 public class Example {
 
     public static void main(String[] args)   {
-        System.out.println("My first program");
+        System.out.println("My test11 program");
         //
         for (int i=0; i<args.length; i++) {
             System.out.println("argument i = " + i + "  value = " + args[i]);  
