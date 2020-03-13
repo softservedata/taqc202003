94ad1155c2fcfb3b492767fe1716ede757b48253
olukom <olukom@softserveinc.com>
2020-03-13 10:16:20 +0200

    Homework changes

--- src/com/softserve/edu/hw2/Appl.java
+++ src/com/softserve/edu/hw2/Appl.java
@@ -2,12 +2,18 @@ package com.softserve.edu.hw2;
 
 public class Appl {
 	public static void main(String args[]) {
+		int k = 16;
 
-        int k=16;
-      
-        int result2 =k++; // 
-        
+		System.out.println(k++);
+		System.out.println(++k);
+		System.out.println(k+++k);
+		System.out.println(50- --k);
+		System.out.println(k*k);
+		System.out.println(k/3);
+		System.out.println(k%3);
+		System.out.println(k++<++k);
+	}
+};
+//
+//
 
-        System.out.println("Result2of k = "  + k );
-      }
-  }
