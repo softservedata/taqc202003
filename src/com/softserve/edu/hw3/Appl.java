77a9789ffdf68cff84523a7ac7f63b50ece8deb7
olukom <olukom@softserveinc.com>
2020-03-15 16:00:05 +0200

    Three values by increasing

--- src/com/softserve/edu/hw3/Appl.java
+++ src/com/softserve/edu/hw3/Appl.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class Appl {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner (System.in);
-		System.out.print(("x="));
-		int x = sc.nextInt();
-		System.out.print(("y="));
-		int y = sc.nextInt();
-		System.out.println("x=" +y );
-		System.out.println("y=" +x );
+		System.out.print(("a="));
+		int a = sc.nextInt();
+		System.out.print(("b="));
+		int b = sc.nextInt();
+		System.out.print(("c="));
+		int c = sc.nextInt();
+		int min= Math.min(a,Math.min(b, c));
+		int max = Math.max(a, Math.max(b, c));
+		int mid = Math.min(max,Math.min(a, Math.max(b, c)));
+		System.out.print(+min );
+		System.out.print(+mid );
+		System.out.print(+max );
 		sc.close();
+	
+}}
 		
-	}
-}
