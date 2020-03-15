12c1b31a574c5d047b35a261504faa294820eb1c
olukom <olukom@softserveinc.com>
2020-03-15 22:10:24 +0200

    entering even numbers

--- src/com/softserve/edu/hw3/Appl.java
+++ src/com/softserve/edu/hw3/Appl.java
@@ -1,23 +1,17 @@
 package com.softserve.edu.hw3;
-
 import java.util.Scanner;
 
 public class Appl {
-	public static void main(String[] args) {
+	
+	static boolean dividesByTwo(Object a ){
 		Scanner sc=new Scanner (System.in);
 		System.out.print(("a="));
-		int a = sc.nextInt();
-		System.out.print(("b="));
-		int b = sc.nextInt();
-		System.out.print(("c="));
-		int c = sc.nextInt();
-		int min= Math.min(a,Math.min(b, c));
-		int max = Math.max(a, Math.max(b, c));
-		int mid = Math.min(max,Math.min(a, Math.max(b, c)));
-		System.out.print(+min );
-		System.out.print(+mid );
-		System.out.print(+max );
-		sc.close();
+		long a1 = sc.nextInt();
+		return (a1 %2==0);
+	}	
+ 
+	public static void main(String[] args){
+		System.out.println(dividesByTwo( "a1"));
+	}
+}
 	
-}}
-		
