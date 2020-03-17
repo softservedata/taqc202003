a7b8bc39233b40e4f12f44f20cd6eba9c517bd63
olukom <olukom@softserveinc.com>
2020-03-17 17:31:03 +0200

    HW3.3 corrected even numbers

--- src/com/softserve/edu/hw3/Appl.java
+++ src/com/softserve/edu/hw3/Appl.java
@@ -3,15 +3,38 @@ import java.util.Scanner;
 
 public class Appl {
 	
-	static boolean dividesByTwo(Object a ){
-		Scanner sc=new Scanner (System.in);
-		System.out.print(("a="));
-		long a1 = sc.nextInt();
-		return (a1 %2==0);
+	public static void main(String[] args) {
+		int a=0;
+		int b=0;
+		int c=0;
+		int d=0;
+		Scanner sc = new Scanner(System.in);{
+			System.out.print("a=");
+			a = sc.nextInt();
+			System.out.print("b=");
+			b = sc.nextInt();
+			System.out.print("c=");
+			c = sc.nextInt();
+			System.out.print("d=");
+			d = sc.nextInt();
+		}
+		if  (a%2==0) {
+			System.out.println("a = even number="+a);
+		}
+			if  (b%2==0) {
+				System.out.println("b= even number="+b);
+			}
+				if  (c%2==0) {
+					System.out.println("c= even number="+c);
+				}
+					if  (d%2==0) {
+						System.out.println("d= even number="+d);
+					}
 	}	
- 
-	public static void main(String[] args){
-		System.out.println(dividesByTwo( "a1"));
 	}
-}
+
+		
+	
+
+	
 	
