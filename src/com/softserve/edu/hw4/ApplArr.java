4a4e25ec681dd44c858167cb922806b0b572a985
olukom <olukom@softserveinc.com>
2020-03-18 21:55:11 +0200

    HW4.1

---
+++ src/com/softserve/edu/hw4/ApplArr.java
@@ -0,0 +1,15 @@
+package com.softserve.edu.hw4;
+
+public class ApplArr {
+	public static void main(String[] args) {
+	int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+	int amount = 0;{
+		int sum =0;
+	for (int a = 0; a<arr.length; a++) {
+		if (a %2==0 ) { sum+= arr[a]; }
+	}
+	System.out.println("Sum = " + sum  );
+
+}
+}
+}
