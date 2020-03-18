8e4a3814c4b76a496bc3e01a78bd809fd18538b2
olukom <olukom@softserveinc.com>
2020-03-18 23:08:09 +0200

    hw4.3

--- src/com/softserve/edu/hw4/Appl2Arr.java
+++ src/com/softserve/edu/hw4/Appl2Arr.java
@@ -4,7 +4,7 @@ public class Appl2Arr {
 	
 
 	public static void main(String[] args) {
-		int[] arr = {0, -1, 2, 3, -4, 5, -6, 7, 8};
+		int[] arr = {0, -1, -2, -3, -4, -5, -6, -7, -8};
 		int max = arr[0];
 		int imax = 0;
 		int i = 0;
---
+++ src/com/softserve/edu/hw4/ApplEmployee.java
@@ -0,0 +1,102 @@
+package com.softserve.edu.hw4;
+import java.util.Scanner;
+
+
+class Employee {
+	private String name;
+	private String department;
+
+	public Employee(String name, String department) {
+
+		this.name = name;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((department == null) ? 0 : department.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+		
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Employee other = (Employee) obj;{
+		if (department == null) {
+			if (other.department != null)
+				return false;
+		
+		} else {if (!department.equals(other.department))
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null)
+		return false;
+		} else { if (!name.equals(other.name))
+			return false;
+		}
+		return true;
+	}
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", department=" + department + "]";
+	}
+
+}
+
+public class ApplEmployee {
+public static void main (String []args) {
+	Employee [] emps = new Employee [5];
+	emps[0]= new Employee ("Olena","Net");
+	emps[1]= new Employee ("Iryna","Shell");
+	emps[2]= new Employee ("Halyna","Net");
+	emps[3]= new Employee ("Vika","RWS");
+	emps[4]= new Employee ("Milena","Reserve");
+	
+	Scanner sc = new Scanner(System.in);{
+		System.out.println("dep =" );
+		String dep =sc.nextLine();
+		for (Employee current:emps) {
+			
+		for (int i=0; i<emps.length; i++) {
+			if (emps[i].getDepartment().trim().equalsIgnoreCase(dep)) {
+				System.out.print(emps[i]);
+			}
+		}
+		}
+	}
+}
+}
+
+		
+	
