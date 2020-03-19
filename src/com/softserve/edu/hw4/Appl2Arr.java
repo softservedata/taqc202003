b9ff754041e1e6a588f20ea3278df2275fd6a842
olukom <olukom@softserveinc.com>
2020-03-19 08:46:10 +0200

    hw4.2

--- src/com/softserve/edu/hw4/Appl2Arr.java
+++ src/com/softserve/edu/hw4/Appl2Arr.java
@@ -7,15 +7,20 @@ public class Appl2Arr {
 	
 		
 		public static void main(String[] args) {
-		int[] arr = { 0, 1, 2, 3, 4, 5,6,7,8,9};
-		int amount = 0;{
-			int sum =0;
-		for (int a = 0; a<arr.length; a++) {
-			if (a %2==0 ) { sum= sum +arr[a] ; }
-		}
-		System.out.println("Sum = "  +sum  );
+		int[] arr = { 0, -1, 2, -3, 8, 5,6,-7,6,1};
+		int min = arr[0];
+		int imin =0;
+			int i =0;
+			while (i<arr.length) {
+				if (arr[i]< min) {
+					min = arr[i];
+					imin=i;
+				}
+		i++;
+			}
+		System.out.println("Minimum = "  +min  );
 
 	}
-		}}
+		}
 
 
