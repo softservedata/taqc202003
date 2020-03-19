81efafecdda9c7e0ebc0915f7d6dee95cf8d17ca
olukom <olukom@softserveinc.com>
2020-03-19 08:05:14 +0200

    hw4.1 corrected

--- src/com/softserve/edu/hw4/Appl2Arr.java
+++ src/com/softserve/edu/hw4/Appl2Arr.java
@@ -1,21 +1,21 @@
 package com.softserve.edu.hw4;
 
+
+
 public class Appl2Arr {
 	
-
-	public static void main(String[] args) {
-		int[] arr = {0, -1, -2, -3, -4, -5, -6, -7, -8};
-		int max = arr[0];
-		int imax = 0;
-		int i = 0;
-		while (i < arr.length) {
-			if (arr[i] < max) {
-				max = arr[i];
-				imax = i;
-			}
-			i++;
+	
+		
+		public static void main(String[] args) {
+		int[] arr = { 0, 1, 2, 3, 4, 5,6,7,8,9};
+		int amount = 0;{
+			int sum =0;
+		for (int a = 0; a<arr.length; a++) {
+			if (a %2==0 ) { sum= sum +a ; }
 		}
-		System.out.print("Maximum = " + max);
-		System.out.println(" is in " + (imax + 1) + " place");
+		System.out.println("Sum = "  +sum  );
+
 	}
-}
+		}}
+
+
