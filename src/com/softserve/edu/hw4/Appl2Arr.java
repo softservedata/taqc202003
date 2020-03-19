545ee08337e480e878f4e2380da1ca478e98221e
olukom <olukom@softserveinc.com>
2020-03-19 09:37:01 +0200

    hw4.2 last change

--- src/com/softserve/edu/hw4/Appl2Arr.java
+++ src/com/softserve/edu/hw4/Appl2Arr.java
@@ -8,19 +8,18 @@ public class Appl2Arr {
 		
 		public static void main(String[] args) {
 		int[] arr = { 0, -1, 2, -3, 8, 5,6,-7,6,1};
-		int min = arr[0];
-		int imin =0;
+		int max = arr[0];
+		int imax =0;
 			int i =0;
 			while (i<arr.length) {
-				if (arr[i]< min) {
-					min = arr[i];
-					imin=i;
+			if(arr[i]< max) {
+					max = arr[i];
+					imax=i;
 				}
 		i++;
 			}
-		System.out.println("Minimum = "  +min  );
-
-	}
+		System.out.println("Maximum = "  +max );
+		}
 		}
 
 
