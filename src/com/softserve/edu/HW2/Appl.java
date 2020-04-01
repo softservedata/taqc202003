7e41798ec677d60c7db54c611242ee39daba8589
mlyn <mlyn@softserveinc.com>
2020-04-01 03:39:48 +0300

    hw2 fix and hw3

--- src/com/softserve/edu/HW2/Appl.java
+++ src/com/softserve/edu/HW2/Appl.java
@@ -1,66 +1,26 @@
-package com.softserve.edu.condition;
-/*
-public class Student {
-	private String name;
-	private int age;
+package com.softserve.edu.HW2;
+import java.util.Scanner;
+/* k++; ++k; k+++k; 50---k; k*k; k/3; k%3; k++<++k; */
+public class Appl {
 
-	public Student(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
 
-	public String getName() {
-		return name;
-	}
+    public static void main(String[] args) {
+         int k =16;
+        System.out.println(" k++ = " + (k++));
+        System.out.println(" ++k = " + (++k));
+        System.out.println("k+++k = " + (k + ++k));
+        System.out.println(" 50---k = " + (50 - --k));
+        System.out.println(" k*k = " + (k*k));
+        System.out.println("k/3 = " + (k/3));
+        System.out.println("k%3 = " + (k%3));
+        System.out.println("k++<++k = " + (k++ < ++k));
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
-	public int getAge() {
-		return age;
-	}
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+        Scanner user_name = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String print_user_name = user_name.nextLine();
+        System.out.println("Nice to meet you, " + print_user_name );
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + age;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (this.getClass() != obj.getClass()) {
-			return false;
-		}
-		Student other = (Student) obj;
-		if (age != other.age) {
-			return false;
-		}
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Student [name=" + name + ", age=" + age + "]";
-	}
-
+    }
 }
-*/
