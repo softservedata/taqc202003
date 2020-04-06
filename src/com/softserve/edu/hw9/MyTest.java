1f56f77c5750f6423f4fe66123dc7f5ab8065357
nnadt <nadtocha.natalya@gmail.com>
2020-04-06 03:38:30 +0300

    HW9

---
+++ src/com/softserve/edu/hw9/MyTest.java
@@ -0,0 +1,51 @@
+package com.softserve.edu.hw9;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MyTest {
+    @Test
+    public void testSearch() {
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://softserve.academy/");
+
+            WebElement link, searchBox, submitBtn;
+
+            link = driver.findElement(By.linkText("Check your knowledge: tests/recommendations"));
+            JavascriptExecutor exec = (JavascriptExecutor)driver;
+            exec.executeScript("arguments[0].click()", link);
+
+            searchBox = driver.findElement(By.name("q"));
+            searchBox.click();
+            searchBox.clear();
+            searchBox.sendKeys("Lviv");
+
+            submitBtn = driver.findElement(By.cssSelector(".btn-secondary"));
+            submitBtn.click();
+
+            List<WebElement> searchResults = driver.findElements(By.xpath("//*[contains(@class,'coursebox')]"));
+
+            List<String> expected = new ArrayList<>(
+                    Arrays.asList("30", "32", "83"));
+            List<String> actual = new ArrayList<>();
+            for (WebElement each : searchResults) {
+                    actual.add(each.getAttribute("data-courseid"));
+                }
+            Collections.sort(actual);
+
+            Assert.assertEquals(expected, actual);
+
+            driver.quit();
+
+            //Result - pass
+    }
+}
