34297800aeddcf5efe75092d6f0a48b1efeb3553
nnadt <nadtocha.natalya@gmail.com>
2020-04-09 09:20:34 +0300

    update to HW10

--- src/com/softserve/edu/hw10/MyTest.java
+++ src/com/softserve/edu/hw10/MyTest.java
@@ -1,24 +1,43 @@
 package com.softserve.edu.hw10;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class CurrencyTest {
-    @Test
-    public void testCurrency() {
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyTest {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
         driver.manage().window().maximize();
-        driver.get("http://taqc-opencart.epizy.com/");
+    }
 
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        driver.quit();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        driver.get("http://taqc-opencart.epizy.com/");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //driver.quit();
+    }
+
+    @Test
+    public void testCurrency() throws Exception {
         driver.findElement(By.cssSelector("button.btn.btn-link.dropdown-toggle")).click();
         driver.findElement(By.cssSelector("[name='GBP']")).click();
 
@@ -35,9 +54,23 @@ public class CurrencyTest {
         for(String a : actualPriceCurrency) {
             Assert.assertTrue(a.contains("£") && !a.contains("$") && !a.contains("€"));
         }
+        //Result - pass
+    }
 
-        driver.quit();
+    @Test
+    public void testSearch() throws Exception {
+        driver.findElement(By.linkText("Laptops & Notebooks")).click();
+        driver.findElement(By.partialLinkText("Macbook")).click();
 
+        List<WebElement> searchResults = driver.findElements(By.xpath("//div[@class='caption']/h4/a"));
+        List<String> actual = new ArrayList<>();
+        for (WebElement each : searchResults) {
+            actual.add(each.getText());
+        }
+
+        for(String a : actual) {
+            Assert.assertTrue(a.toLowerCase().contains("macbook"));
+        }
         //Result - pass
     }
 }
