c524944689e3f6bb12ed4310270fec75a65aa2b7
oleksiy.makhynia <oleksiy.makhynia@solarwinds.com>
2020-04-20 09:28:31 +0300

    HW#13

---
+++ src/test/java/com/softserve/edu/hw13/productdata/ProductItems.java
@@ -0,0 +1,17 @@
+package com.softserve.edu.hw13.productdata;
+
+public class ProductItems {
+			
+	private String product;
+	//TODO
+
+	public ProductItems(String product) {
+		this.product = product;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+	
+	
+}
---
+++ src/test/java/com/softserve/edu/opencart/data/SearchItem.java
@@ -0,0 +1,16 @@
+package com.softserve.edu.opencart.data;
+
+public class SearchItem {
+
+	private String searchText;
+	// TODO
+
+	public SearchItem(String searchText) {
+		this.searchText = searchText;
+	}
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+}
